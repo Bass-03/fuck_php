f177d8b8746e6f923a578bdba252010342ba2d97
Edmundo Sanchez <zomundo@gmail.com>
2018-10-26 00:25:15 -0500

    fixed string ending on aliases

--- fuck-php.sh
+++ fuck-php.sh
@@ -5,9 +5,9 @@ filerc=$1
 
 ## Apend to rc file
 echo '\n# fuck-php' | tee -a $filerc
-echo 'alias use_php7.0="sudo update-alternatives --set php /usr/bin/php7.0" && php --version' | tee -a $filerc
-echo 'alias use_php7.1="sudo update-alternatives --set php /usr/bin/php7.1" && php --version' | tee -a $filerc
-echo 'alias use_php7.2="sudo update-alternatives --set php /usr/bin/php7.2" && php --version' | tee -a $filerc
+echo 'alias use_php7.0="sudo update-alternatives --set php /usr/bin/php7.0 && php --version"' | tee -a $filerc
+echo 'alias use_php7.1="sudo update-alternatives --set php /usr/bin/php7.1 && php --version"' | tee -a $filerc
+echo 'alias use_php7.2="sudo update-alternatives --set php /usr/bin/php7.2 && php --version"' | tee -a $filerc
 
 #set alias now too
 alias use_php7.0="sudo update-alternatives --set php /usr/bin/php7.0 && php --version"
