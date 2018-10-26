1ba13755aa126d92d4512a72c931d5f1bffad876
Edmundo Sanchez <zomundo@gmail.com>
2018-10-26 00:07:26 -0500

    fuck php aliases

---
+++ fuck-php.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Get zshrc file or similar
+filerc=$1
+
+## Apend to rc file
+echo '# fuck-php' | tee -a $filerc
+echo 'alias use_php7.0="sudo update-alternatives --set php /usr/bin/php7.0" && php --version' | tee -a $filerc
+echo 'alias use_php7.1="sudo update-alternatives --set php /usr/bin/php7.1" && php --version' | tee -a $filerc
+echo 'alias use_php7.2="sudo update-alternatives --set php /usr/bin/php7.2" && php --version' | tee -a $filerc
+
+#set alias now too
+alias use_php7.0="sudo update-alternatives --set php /usr/bin/php7.0 && php --version"
+alias use_php7.1="sudo update-alternatives --set php /usr/bin/php7.1 && php --version"
+alias use_php7.2="sudo update-alternatives --set php /usr/bin/php7.2 && php --version"
